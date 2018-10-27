8a3255176e63abc3c6fec24e73fd9ef7269c0278
Maksym Biloglazov <fenixx742@gmail.com>
2018-10-28 00:55:23 +0300

    Task 3 done with boolean

--- task3/IntCheck.java
+++ task3/IntCheck.java
@@ -2,6 +2,6 @@ package com.hillel.task3;
 
 public class IntCheck {
     public static boolean evenNumCheck (int z) {
-        return z % 2 == 0;
+        return z % 2 == 0; // check is a number have remainder of division
     }
 }
