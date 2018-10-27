a1c2dd46cf4b388a7b6f4de8071f5bbdf8c57ce2
Maksym Biloglazov <fenixx742@gmail.com>
2018-10-28 00:49:17 +0300

    Task 3 done with boolean

---
+++ task3/EvenCheck.java
@@ -0,0 +1,7 @@
+package com.hillel.task3;
+
+public class EvenCheck {
+    public static void main (String args[]) {
+        System.out.println(" is 4 is even? it`s " + IntCheck.evenNumCheck(4));
+    }
+}
---
+++ task3/IntCheck.java
@@ -0,0 +1,7 @@
+package com.hillel.task3;
+
+public class IntCheck {
+    public static boolean evenNumCheck (int z) {
+        return z % 2 == 0;
+    }
+}
