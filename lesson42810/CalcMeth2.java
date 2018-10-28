1c245c24f532bf7c845d51ed020a6952c8c37f5f
Maksym Biloglazov <fenixx742@gmail.com>
2018-10-28 12:40:12 +0200

    lesson 4 bit wise ops *, OR &, XOR |, shift >> << if & else construction

---
+++ lesson42810/CalcMeth2.java
@@ -0,0 +1,27 @@
+package com.hillel.lesson42810;
+
+public class CalcMeth2 {
+    public static void bitWiseOps() {
+        int i = 7;
+        int result = i & 1;
+        System.out.println(result);
+    }
+
+
+    public static void isEvenNum(int i) {
+        int result = i & 1;
+        if (result == 0)
+        System.out.println(result + " 100 is even");
+        else {
+            if (result != 0)
+                System.out.println(result + " is odd");
+        }
+
+    }
+    public static int shiftRigth(int num, int x) {
+        //int result = num >> x;  // shifting right in bits
+        //return result;
+        return num >> x; // shorter version
+    }
+}
+
---
+++ lesson42810/MainCalc2.java
@@ -0,0 +1,10 @@
+package com.hillel.lesson42810;
+
+
+public class MainCalc2 {
+    public static void main(String args[]) {
+        CalcMeth2.bitWiseOps();
+        CalcMeth2.isEvenNum(101);
+        System.out.println(CalcMeth2.shiftRigth(120,4) );
+    }
+}
