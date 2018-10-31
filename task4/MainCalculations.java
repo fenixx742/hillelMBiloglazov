659d79f2db312510a7471ebfeb284743fa559049
Maksym Biloglazov <fenixx742@gmail.com>
2018-10-31 17:18:24 +0200

    task 4 without last subtask

--- task4/MainCalculations.java
+++ task4/MainCalculations.java
@@ -4,12 +4,20 @@ public class MainCalculations {
     public static void main (String args[]) {
         System.out.println("2240 UAH is " + UahToUsdConv.usDollarsConv(28.5, 2240) + " dollars"); // Converter
 
-        System.out.println(SummationNums.sumNum(123) + " summation of 123"); // three numbers summition
+        System.out.println(SummationNums.sumNum(123) + " is summation of 123"); // three numbers summition
+
+        System.out.println();
+
+        System.out.println("<>Garden calculation<>");
 
         System.out.println("Fence needed for garden is " + ZaborGazonCalc.perimCalc(2,4) + " meters");
 
         System.out.println("Garden grass needed  " + ZaborGazonCalc.gardenCalSqr(2,4) + " m2");
 
-        TrainsCollision.trains(100, 100);
+        System.out.println();
+
+        System.out.println("<>Train Collision Check<>");
+
+        TrainsCollision.trains(50, 61);
     }
 }
