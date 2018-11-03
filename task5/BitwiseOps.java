4f059158a8cbd0d8ef00f75d974666a6ee7f5b11
Maksym Biloglazov <fenixx742@gmail.com>
2018-11-03 23:44:45 +0200

    task 5 edited

--- task5/BitwiseOps.java
+++ task5/BitwiseOps.java
@@ -2,7 +2,7 @@ package com.hillel.task5;
 
 public class BitwiseOps {
     public static long bitDivByFour(double number) {
-        long result = (byte) number >> 2;
+        long result = (long) number >> 2;
         return result;
     }
 }
--- task5/SeasonCheckBySwitch.java
+++ task5/SeasonCheckBySwitch.java
@@ -7,40 +7,24 @@ public class SeasonCheckBySwitch {
 
         switch (a) {
             case 1:
-                System.out.println("It is Winter");
-                break;
             case 2:
+            case 12:
                 System.out.println("It is Winter");
                 break;
             case 3:
-                System.out.println("It is Spring");
-                break;
             case 4:
-                System.out.println("It is Spring");
-                break;
             case 5:
                 System.out.println("It is Spring");
                 break;
             case 6:
-                System.out.println("It is Summer");
-                break;
             case 7:
-                System.out.println("It is Summer");
-                break;
             case 8:
                 System.out.println("It is Summer");
                 break;
             case 9:
-                System.out.println("It is Autumn");
-                break;
             case 10:
-                System.out.println("It is Autumn");
-                break;
             case 11:
                 System.out.println("It is Autumn");
-                break;
-            case 12:
-                System.out.println("It is Winter");
                 break;
             default:
                 System.out.println("Error Error");
