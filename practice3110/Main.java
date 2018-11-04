afae238054c714b4b074c9288e6e4f89119c677e
Maksym Biloglazov <fenixx742@gmail.com>
2018-11-04 10:52:00 +0200

    practice 04/11

---
+++ practice3110/Main.java
@@ -0,0 +1,21 @@
+package com.hillel.practice3110;
+
+import com.sun.tools.javac.code.Attribute;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        System.out.println("Random nums");
+
+        double d1 = (double)(Math.random()) * 100;
+        System.out.println(d1);
+        double d2 = (double)(Math.random()) * 100;
+        System.out.println(d2);
+        double d3 = (double)(Math.random()) * 100;
+        System.out.println(d3);
+
+
+
+    }
+}
