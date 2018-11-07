5e5f74bc12171fbd18b9774ff2e22844041e7d9d
Maksym Biloglazov <fenixx742@gmail.com>
2018-11-07 17:16:51 +0200

    task 6 done

--- task6/BitwiseViewPrintOut.java
+++ task6/BitwiseViewPrintOut.java
@@ -2,15 +2,14 @@ package com.hillel.task6;
 
 public class BitwiseViewPrintOut {
     public static void bitLookMethod () {
-        System.out.print(1);
-            for (int j = 0; j < 4 ; j++) {
-                System.out.print(0);
-            }
-        System.out.println();
-
-            for (int i = 0; i <= 4; i++) {
-                System.out.print(01);
-
-        }
-    }
+            for (int j = 0; j < 5 ; j++) {
+                for (int i = 0; i < 5; i++) {
+                    if (j == i) {
+                        System.out.print(1);
+                        continue;
+                    } else
+                        System.out.print(0);
+                } System.out.println();
+          }
+     }
 }
