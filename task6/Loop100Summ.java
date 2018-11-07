fb97ad9293680bcf3148c6639d8e81c7f89979a0
Maksym Biloglazov <fenixx742@gmail.com>
2018-11-07 08:45:54 +0200

    task 6

---
+++ task6/Loop100Summ.java
@@ -0,0 +1,13 @@
+package com.hillel.task6;
+
+public class Loop100Summ {
+    public static void loopForSumm (int min, int max){
+        int sum = 0;
+            for (int i = min; i <= max; i++) {
+
+                  sum += i;
+
+                    System.out.println(sum);
+            }
+      }
+}
---
+++ task6/MainStarter.java
@@ -0,0 +1,17 @@
+package com.hillel.task6;
+
+import com.hillel.Utils.ForLoop;
+import com.hillel.Utils.MathRandomNums;
+public class MainStarter {
+    public static void main(String args[]) {
+        //Loop100Summ.loopForSumm(1,100); // sum of 1 to 100
+
+        //ForLoop.loop(1,100); // odd numbers  print out 1 to 100
+
+        //EaglesAndTails.randomCalc();
+
+        EaglesAndTails.randomCalc();
+
+
+    }
+}
