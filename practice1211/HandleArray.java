c38183f9d20d0ea5bf41056484b0b243d902f138
Maksym Biloglazov <fenixx742@gmail.com>
2018-11-11 12:00:39 +0200

    q

--- practice1211/HandleArray.java
+++ practice1211/HandleArray.java
@@ -1,10 +1,15 @@
 package com.hillel.practice1211;
 
 public class HandleArray {
-    public static int[] raiseSalary(int[] a, int percent) {
+    public static int[] raiseSalary1(int[] a, int percent) {
         int [] updatedSalary = new int [a.length];
         for (int i = 0; i < a.length ; i++) {
             updatedSalary [i] = (int) (a[i] * (double)percent / 100) + a[i];
         } return updatedSalary;
+    }
+    public static void raiseSalary(int[] a, int percent) {
+        for (int i = 0; i < a.length ; i++) {
+          a [i] = (int) (a[i] * (double)percent / 100) + a[i];
+        }
     }
 }
--- practice1211/MainPractice1211.java
+++ practice1211/MainPractice1211.java
@@ -2,25 +2,32 @@ package com.hillel.practice1211;
 
 public class MainPractice1211 {
     public static void main(String[] args) {
-        int [] arr = {-10, 32, 343, -100, 1310};
+//        int [] arr = {-10, 32, 343, -100, 1310};
 //        double [] dArr = {2.0, 2.54, 32.3, 32};
 //
 //      int minimum = ArrayMinMaxNums.getMin(arr);
 //        System.out.println(minimum);
 //      double dMinimum = ArrayMinMaxNums.getMin(dArr);
 //        System.out.println(dMinimum);
+
         int [] arr1 = {-10, 3, 34, -100, 110};
         ShowArray.showArray(arr1);
+
         HandleArray.raiseSalary(arr1, 20);
+        System.out.println("после увелицения на 20");
         ShowArray.showArray(arr1);
 
-        int [] a = new int [10];
-        double[] d = new double[5];
-        boolean[] f = new boolean[3];
-        int [] r = new int [] {1,2,3,4,5};
+       int [] draftSalary = HandleArray.raiseSalary1(arr1, 30);
+        System.out.println("после увелицения на 30");
+        ShowArray.showArray(draftSalary);
 
-        a[0] = 5;
-        a[10] = 100;
+//        int [] a = new int [10];
+//        double[] d = new double[5];
+//        boolean[] f = new boolean[3];
+//        int [] r = new int [] {1,2,3,4,5};
+//
+//        a[0] = 5;
+//        a[10] = 100;
 
         }
 
