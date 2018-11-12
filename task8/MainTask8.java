b46565fae40d68b13a6c3736f31f072490aae763
Maksym Biloglazov <fenixx742@gmail.com>
2018-11-12 18:19:09 +0200

    task 8

--- task8/MainTask8.java
+++ task8/MainTask8.java
@@ -3,14 +3,15 @@ package com.hillel.task8;
 public class MainTask8 {
     public static void main(String[] args) {
 
+        GuessGame2.getRandomNumAndGuess();
+        System.out.println();
         System.out.println("countdown");
        Countdown.getCount();
         System.out.println("progression");
        Progression.getProgress();
         System.out.println();
         Factor.getFactorial();
-        GuessGame2.getRandomNumAndGuess();
-        System.out.println();
+
 
     }
 }
