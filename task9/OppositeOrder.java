211b2a33dbe49b8f78d8b765c21bcd5086b62990
Maksym Biloglazov <fenixx742@gmail.com>
2018-11-14 14:07:49 +0200

    task 9
    Signed-off-by: Maksym Biloglazov <fenixx742@gmail.com>

--- task9/OppositeOrder.java
+++ task9/OppositeOrder.java
@@ -2,7 +2,6 @@ package com.hillel.task9;
 
 public class OppositeOrder {
     public static void getOpposite(int[] a) {
-
         for (int i = a.length - 1; i >= 0 ; i--) {
             System.out.print(a[i] + " ");
         }
